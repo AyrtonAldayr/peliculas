6482d12ab53ec593ae5aa21c6d361092da794c3d
AyrtonAldayrNevesEstrada <ayrton.24.6.95@gmail.com>
2020-04-05 04:15:59 -0500

    detectando el click de las peliculas horizontales

--- lib/widgets/movi_horizontal.dart
+++ lib/widgets/movi_horizontal.dart
@@ -38,7 +38,7 @@ class MovideHorizontal extends StatelessWidget {
   }
 
   Widget _tarjeta(BuildContext context, Pelicula pelicula){
-    return Container(
+    final tarjeta = Container(
       margin: EdgeInsets.only(right: 15.0),
       child: ListView(
         scrollDirection: Axis.vertical,
@@ -61,33 +61,12 @@ class MovideHorizontal extends StatelessWidget {
         ],
       ),
     );
-  }
 
-  List<Widget> _tarjetas(BuildContext context){
-    return peliculas.map((pelicula){
-      return Container(
-        margin: EdgeInsets.only(right: 15.0),
-        child: ListView(
-          scrollDirection: Axis.vertical,
-          children: <Widget>[
-            ClipRRect(
-              borderRadius: BorderRadius.circular(20.0),
-              child: FadeInImage(
-                image: NetworkImage(pelicula.getPosterImg()),
-                placeholder: AssetImage('assets/images/no-image.jpg'),
-                fit: BoxFit.cover,
-                height: 160.0,
-              ),
-            ),
-            SizedBox(height: 5.0,),
-            Text(
-              pelicula.title,
-              overflow: TextOverflow.ellipsis,
-              style: Theme.of(context).textTheme.caption,
-            )
-          ],
-        ),
-      );
-    }).toList();
+    return GestureDetector(
+      child: tarjeta,
+      onTap: (){
+        print('Id de la pelicula ${pelicula.id} ${pelicula.title}');
+      },
+    );
   }
 }
